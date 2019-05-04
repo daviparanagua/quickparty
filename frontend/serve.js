a2ecbae551d447fedc480d19e1dfd7d9b8fec9ab
Davi Paranaguá <daviparanagua@gmail.com>
2019-05-04 15:36:56 -0300

    Ajustes ao servidor de front

--- frontend/serve.js
+++ frontend/serve.js
@@ -3,20 +3,46 @@ const fs = require('fs');
 const http = require('http');
 const https = require('https');
 const express = require('express');
-const dotenv = require('dotenv').config();
+require('dotenv').config();
 
 const app = express();
 
-// Certificate
-const privateKey = fs.readFileSync(process.env.SSL_PRIVATE_KEY, 'utf8');
-const certificate = fs.readFileSync(process.env.SSL_CERTIFICATE, 'utf8');
-const ca = fs.readFileSync(process.env.SSL_CA, 'utf8');
+const devMode = process.argv[2] === 'dev';
 
-const credentials = {
-  key: privateKey,
-  cert: certificate,
-  ca: ca
-};
+// SSL test
+
+const useSSL = !devMode;
+let credentials = {};
+let truePort = 80;
+
+if (useSSL) {
+  // Certificate
+  const privateKey = fs.readFileSync(process.env.SSL_PRIVATE_KEY, 'utf8');
+  const certificate = fs.readFileSync(process.env.SSL_CERTIFICATE, 'utf8');
+  const ca = fs.readFileSync(process.env.SSL_CA, 'utf8');
+
+  credentials = {
+    key: privateKey,
+    cert: certificate,
+    ca: ca
+  };
+
+  truePort = 443;
+  // REDIRECT SERVER : CREATES A FAKE PORT 80 SERVER TO REDIRECT TO HTTPS
+
+  let redirectApp = express();
+
+  redirectApp.use(function (req, res) {
+    var fullUrl = 'https://' + req.get('host') + req.originalUrl;
+    res.redirect(fullUrl);
+  });
+
+  const redirectServer = http.createServer(redirectApp);
+
+  redirectServer.listen(80, () => {
+    console.log(`Redirect HTTP Server running on port 80`);
+  });
+}
 
 app.use(express.static('dist/pwa'));
 
@@ -24,24 +50,9 @@ app.use(function (req, res) {
   res.sendFile('dist/pwa/index.html', { root: __dirname });
 });
 
-// Starting both http & https servers
-const httpsServer = https.createServer(credentials, app);
+// Starting true server
+const trueServer = useSSL ? https.createServer(credentials, app) : http.createServer(app);
 
-httpsServer.listen(443, () => {
-  console.log('HTTPS Server running on port 443');
-});
-
-// REDIRECT SERVER
-
-let redirectApp = express();
-
-redirectApp.use(function (req, res) {
-  var fullUrl = 'https://' + req.get('host') + req.originalUrl;
-  res.redirect(fullUrl);
-});
-
-const httpServer = http.createServer(redirectApp);
-
-httpServer.listen(80, () => {
-  console.log('HTTP Server running on port 80');
+trueServer.listen(truePort, () => {
+  console.log(`App Server running on port ${truePort}`);
 });
